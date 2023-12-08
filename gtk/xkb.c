a0db0135c271be03c0693ea6e5990e9ed3c0cb79
aliriza <aliriza.keskin@pardus.org.tr>
2023-12-08 11:12:40 +0000

    convert string to label

--- gtk/xkb.c
+++ gtk/xkb.c
@@ -2,7 +2,10 @@
 #include <X11/XKBlib.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#define iseq(A,B) strcmp(A,B)==0
+static char* str_to_label(char* str);
 Display *display;
 
 char* get_label_from_keycode(int code, int group){
@@ -16,9 +19,54 @@ char* get_label_from_keycode(int code, int group){
     KeySym keysym = XkbKeycodeToKeysym(display, code, group, 0);
     char* sym = XKeysymToString(keysym);
     if(sym){
-        return sym;
+        return str_to_label(sym);
     }else{
         return "";
     }
 }
 
+static char* str_to_label(char* str){
+    if(iseq(str,"grave")){
+        return "`";
+    }else if(iseq(str,"minus")){
+        return "-";
+    }else if(iseq(str,"equal")){
+        return "=";
+    }else if(iseq(str,"bracketleft")){
+        return "[";
+    }else if(iseq(str,"bracketright")){
+        return "]";
+    }else if(iseq(str,"semicolon")){
+        return ";";
+    }else if(iseq(str,"apostrophe")){
+        return "'";
+    }else if(iseq(str,"backslash")){
+        return "\\";
+    }else if(iseq(str,"comma")){
+        return ",";
+    }else if(iseq(str,"period")){
+        return ".";
+    }else if(iseq(str,"slash")){
+        return "/";
+    }else if(iseq(str,"quotedbl")){
+        return "\"";
+    }else if(iseq(str,"asterisk")){
+        return "*";
+    }else if(iseq(str,"idotless")){
+        return "ı";
+    }else if(iseq(str,"gbreve")){
+        return "ğ";
+    }else if(iseq(str,"udiaeresis")){
+        return "ü";
+    }else if(iseq(str,"scedilla")){
+        return "ş";
+    }else if(iseq(str,"odiaeresis")){
+        return "ö";
+    }else if(iseq(str,"ccedilla")){
+        return "ç";
+    }else if(iseq(str,"schwa")){
+        return "ə";
+    }else{
+        return str;
+    }
+}
