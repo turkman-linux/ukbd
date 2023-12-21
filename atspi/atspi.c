737bfe68028379cc5a9420e70f4f79fbf9fe83c3
aliriza <aliriza.keskin@pardus.org.tr>
2023-12-21 14:34:06 +0000

    atspi improvements

--- atspi/atspi.c
+++ atspi/atspi.c
@@ -4,10 +4,7 @@
 #include <string.h>
 
 static void on_event (const AtspiEvent *event, void *data) {
-  if(fork()){
-      char** args = {"ukbd-gtk", NULL};
-      execvp("/usr/bin/ukbd-gtk", args);
-  }
+  system("ukbd-gtk &");
 }
 
 int main (int argc, gchar **argv) {
@@ -15,7 +12,6 @@ int main (int argc, gchar **argv) {
   atspi_init ();
   listener = atspi_event_listener_new (on_event, NULL, NULL);
   atspi_event_listener_register (listener, "object:state-changed:focused", NULL);
-  atspi_event_listener_register (listener, "object:state-changed:selected", NULL);
   atspi_event_main ();
   return 0;
 }
