614481f9862f11710340927926b39c0d946a22cb
aliriza <aliriza.keskin@pardus.org.tr>
2024-04-03 09:55:50 +0000

    fix keytab generate issue

--- gtk/generate_keytab.sh
+++ gtk/generate_keytab.sh
@@ -4,10 +4,10 @@ echo '#define iseq(A,B) strcmp(A,B)==0'
 echo '#include <string.h>'
 echo 'char* str_to_label(char* str) {'
 set -o pipefail
-cat /usr/include/X11/keysymdef.h | grep "^#define " | tr -s " " | while read line ; do
+cat /usr/include/X11/keysymdef.h | grep "^#define " | tr -s " "  | grep -v Return | while read line ; do
     label=$(echo "$line" | cut -f2 -d" " | sed "s/^XK_//g")
-    code=$(echo "$line" | cut -f5 -d" " | grep "^U+")
-    code=$(printf "${code/U+/"\U"}")
+    code=$(echo "$line" | cut -f3 -d" " | grep "^0x")
+    code=$(printf "${code/0x/"\U"}")
     if echo $line | grep "deprecated" >/dev/null ; then
         echo "    str_check(str, \"$label\", \"\");"
     elif [[ "$code" != "" && "$code" != "$label" ]] ; then
